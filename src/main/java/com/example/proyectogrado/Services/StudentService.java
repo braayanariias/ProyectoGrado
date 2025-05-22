1e8a053055270702c1a0b7df26421a95339a2e1c
braayanariias <brayanarias19@outlook.es>
2025-05-22 13:57:14 -0500

    refactor(StudentService): mejora injeccion por constructor para StudentRepository y añade doc swagger-ui

--- src/main/java/com/example/proyectogrado/Services/StudentService.java
+++ src/main/java/com/example/proyectogrado/Services/StudentService.java
@@ -1,9 +1,27 @@
 package com.example.proyectogrado.Services;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+
+import com.example.proyectogrado.Models.Student;
+import com.example.proyectogrado.Repositorys.StudentRepository;
 
 @Service
 public class StudentService {
-    
+
+    private StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student getStudentById(UUID id) {
+        return studentRepository.findById(id).orElse(null);
+    }
     
 }
