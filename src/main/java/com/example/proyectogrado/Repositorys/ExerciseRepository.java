b7cfe7b720270e8441553c81a4e8dcfed8d03c1e
braayanariias <brayanarias19@outlook.es>
2026-09-02 21:42:23 -0500

    Eliminar anotacion innecesaria

--- src/main/java/com/example/proyectogrado/Repositorys/ExerciseRepository.java
+++ src/main/java/com/example/proyectogrado/Repositorys/ExerciseRepository.java
@@ -3,12 +3,10 @@ package com.example.proyectogrado.Repositorys;
 import com.example.proyectogrado.Models.Exercise;
 import com.example.proyectogrado.Models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
-@Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, UUID> {
     
     List<Exercise> findByStudent(Student student);
--- src/main/java/com/example/proyectogrado/Repositorys/SolutionRepository.java
+++ src/main/java/com/example/proyectogrado/Repositorys/SolutionRepository.java
@@ -3,17 +3,14 @@ package com.example.proyectogrado.Repositorys;
 import com.example.proyectogrado.Models.Solution;
 import com.example.proyectogrado.Models.Student;
 import com.example.proyectogrado.Models.Exercise;
-import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface SolutionRepository extends JpaRepository<Solution, UUID> {
     
     List<Solution> findByStudentId(UUID studentId);
